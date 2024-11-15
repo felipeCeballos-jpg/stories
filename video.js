fc3ecb24f2a0a4c4cf1806984e108434c0fa2ed5
felipeceballos12 <ceballos-65@hotmail.com>
2024-11-14 22:20:58 -0500

    Just leave how it is

--- video.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const videoContainer = document.querySelector('.loader');
-  videoContainer.innerHTML = `
-      <video loop muted autoplay playsinline>
-    <source src="./assets/loader.mp4" type="video/mp4" />
-  </video>
-    `;
-});
